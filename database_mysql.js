6fb59882f993d71d08e1bca4f077f632c7ca5b5b
이아현 <57928612+LAH1203@users.noreply.github.com>
2021-01-27 17:12:17 +0900

    Update database_mysql.js

--- database_mysql.js
+++ database_mysql.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  // password: 'password',
   database: 'assignment_3'
 });
 
